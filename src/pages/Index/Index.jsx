e649a00e405620a56eefe1150452216aaa6bd44d
chenshuimu <623242742@qq.com>
2023-09-27 18:06:38 +0800

    feat: 优化代码

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,48 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { Button } from 'antd-mobile'
+import { Button, Modal } from 'antd-mobile'
 import styles from './index.css'
 
 import trueIcon from '../../images/trueIcon.png'
 import falseIcon from '../../images/falseIcon.png'
 
 function Index() {
+  const numberOfQuestions = 50; // 问题数量
+  const numberMax = 100; // 生成的随机数最大值
   const [questions, setQuestions] = useState([]);
-  const [answers, setAnswers] = useState([]);
   const [submitted, setSubmitted] = useState(false);
-  const [userAnswers, setUserAnswers] = useState(new Array(50).fill(''));
-  const [results, setResults] = useState(new Array(50).fill(null));
+  const [userAnswers, setUserAnswers] = useState(new Array(numberOfQuestions).fill(''));
 
   // 生成随机的问题和答案
-  const generateRandomNumbers = () => {
-    const numberOfQuestions = 50; // 修改为您希望生成的问题数量
-    const newQuestions = [];
-    const newAnswers = [];
-
-    for (let i = 0; i < numberOfQuestions; i++) {
-      let num1 = Math.floor(Math.random() * 100) + 1; // 生成 1 到 100 的随机数
-      let num2 = Math.floor(Math.random() * 100) + 1;
-      if (num1 < num2) {
-        // 如果num1小于num2，交换它们的值
-        const temp = num1;
-        num1 = num2;
-        num2 = temp;
-      }
-      let correctAnswer = num1 + num2;
-      let quesText = `${num1} + ${num2} = `
-      if(Math.random() < 0.5) {
-        correctAnswer = num1 - num2;
-        quesText = `${num1} - ${num2} = `
-      }
+  const generateRandomNumbers = (isScroll) => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // 使用平滑滚动效果
+    });
+    const newQuestions = Array.from({ length: numberOfQuestions }, () => {
+      const num1 = Math.floor(Math.random() * numberMax) + 1; // 生成 1 到 numberMax 的随机数
+      const num2 = Math.floor(Math.random() * numberMax) + 1;
+      const isSubtraction = Math.random() < 0.5; // 是否为减法
+      // 数字1比数字2小且为减法的时候需要对调两个数字
+      const [larger, smaller] = num1 < num2 && isSubtraction ? [num2, num1] : [num1, num2];
+      const correctAnswer = isSubtraction ? larger - smaller : larger + smaller;
+      const quesText = isSubtraction ? `${larger} - ${smaller} = ` : `${larger} + ${smaller} = `;
       
-      newQuestions.push(quesText);
-      newAnswers.push(correctAnswer);
-    }
-
+      return { question: quesText, answer: correctAnswer };
+    });
     setQuestions(newQuestions);
-    setAnswers(newAnswers);
     setSubmitted(false);
     setUserAnswers(new Array(numberOfQuestions).fill(''));
-    setResults(new Array(numberOfQuestions).fill(null));
   };
   
   useEffect(() => {
@@ -52,32 +41,38 @@ function Index() {
 
   const handleSubmit = () => {
     // 计算每个问题的结果并判断得分
-    const newResults = answers.map((correctAnswer, index) => {
+    const newResults = questions.map((item, index) => {
       const userAnswer = parseInt(userAnswers[index], 10);
-      return userAnswer === correctAnswer;
+      return userAnswer === item.answer;
     });
-
-    console.log(newResults,'newResults');
 
     // 计算正确答案的数量
     const count = newResults.filter(item => item === true).length;
-    console.log(count);
-
+    let contentText;
     // 根据得分弹出不同的提示
-    if(count < count.length * 0.6) {
-      alert('不及格哦，继续努力');
-    } else if(count === count.length) {
-      alert('太棒啦，你获得了满分！');
+    if(count < questions.length * 0.6) {
+      contentText = '不及格哦，继续努力';
+    } else if(count === questions.length) {
+      contentText = '太棒啦，你获得了满分！';
     } else {
-      alert(`恭喜你获得${count}分，继续加油吧`);
+      contentText = `恭喜你获得${count}分，继续加油吧`;
     }
 
+    Modal.alert({
+      content: (
+        <div style={{textAlign: 'center'}}>
+          {contentText}
+        </div>
+      ),
+      closeOnMaskClick: true,
+    })
+
     // 更新结果状态
-    setResults(newResults);
     setSubmitted(true);
   };
 
   const handleUserAnswerChange = (index, value) => {
+    if(value > 999) return; // 限制最大值999
     // 处理用户输入的答案
     const newAnswers = [...userAnswers];
     newAnswers[index] = value;
@@ -87,10 +82,10 @@ function Index() {
   return (
     <div className={styles.main}>
       <h1>小学一年级加法试卷</h1>
-      {questions.map((question, index) => (
+      {questions.map((item, index) => (
         <div key={index}>
           <div className={styles.question}>
-            <div>{question}</div>
+            <div>{item?.question}</div>
             {!submitted && (
               <input
                 className={styles.answerInput}
@@ -101,21 +96,21 @@ function Index() {
             )}
             {submitted && (
               <div className={styles.answerText}>
-                答案：{answers[index]}
+                答案：{item.answer}
               </div>
             )}
           </div>
           { submitted && (
-            <div className={styles.youAnswer} style={{color: results[index] ? '#2bb66d' : '#da2a2a'}}>
+            <div className={styles.youAnswer} style={{color: parseInt(userAnswers[index]) === item.answer ? '#2bb66d' : '#da2a2a'}}>
               你的答案：{userAnswers[index]}
-              <img src={results[index]  ? trueIcon : falseIcon} alt="" />
+              <img src={parseInt(userAnswers[index]) === item.answer ? trueIcon : falseIcon} alt="" />
             </div>
           )}
         </div>
       ))}
       <br />
       {!submitted ? <Button color='success' fill='solid' className={styles.submitBtn} onClick={handleSubmit}>提交答案</Button>
-      : <Button className={styles.submitBtn}color='warning' onClick={generateRandomNumbers}>重新生成试卷</Button>}
+      : <Button className={styles.submitBtn}color='warning' onClick={()=>{generateRandomNumbers(true)}}>重新生成试卷</Button>}
     </div>
   );
 }
